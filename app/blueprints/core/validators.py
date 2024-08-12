3795e728098345aee6dc2d56ce942f6d2149e3bf
payam <payamdvlp007@gmail.com>
2024-08-12 14:32:41 +0330

    chore: added comparison of date_to and date_from

--- app/blueprints/core/validators.py
+++ app/blueprints/core/validators.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 
 def validate(date_from: str, date_to: str, origin: str, destination: str) -> list[str]:
@@ -17,20 +18,34 @@ def validate(date_from: str, date_to: str, origin: str, destination: str) -> lis
     region_code_pattern = re.compile(r"^[A-Z]{5}$")
     slug_pattern = re.compile(r"^[a-z]+(_[a-z]+)*$")
 
+    # Handling date validation
+    date_format_is_valid = True
     if date_from and not date_pattern.match(date_from):
         errors.append("date_from format is not valid")
+        date_format_is_valid = False
     if date_to and not date_pattern.match(date_to):
         errors.append("date_from format is not valid")
-    if date_from == date_to:
-        errors.append("date_from and date_to couldn't be same!")
+        date_format_is_valid = False
+    if not bool(date_to):
+        errors.append("date_to should be determined")
+        date_format_is_valid = False
+    if not bool(date_from):
+        errors.append("date_from should be determined")
+        date_format_is_valid = False
+
+    if date_format_is_valid:  # This part is processed only if date format are valid
+        if date_from == date_to:
+            errors.append("date_from and date_to couldn't be same!")
+        if datetime.strptime(date_from, "%Y-%m-%d") > datetime.strptime(
+            date_to, "%Y-%m-%d"
+        ):
+            errors.append("date_from could not be greater than date_to")
+
+    # Handling origin and destination validation
     if not bool(origin):
         errors.append("origin should be determined")
     if not bool(destination):
         errors.append("destination should be determined")
-    if not bool(date_to):
-        errors.append("date_to should be determined")
-    if not bool(date_from):
-        errors.append("date_from should be determined")
 
     if bool(origin) and not (
         region_code_pattern.match(origin) or slug_pattern.match(origin)
